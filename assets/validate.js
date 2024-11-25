8df87892c15abeae32891e6cd879cd4693bd2a9d
hollali <dheztinykartel@gmail.com>
2024-11-25 13:18:39 +0000

    All

--- assets/validate.js
+++ assets/validate.js
@@ -1,40 +1,39 @@
 /* <![CDATA[ */
 // Jquery validate form contact
-jQuery(document).ready(function(){
+jQuery(document).ready(function () {
+	$("#contactform").submit(function () {
+		var action = $(this).attr("action");
 
-	$('#contactform').submit(function(){
+		$("#message-contact").slideUp(750, function () {
+			$("#message-contact").hide();
 
-		var action = $(this).attr('action');
+			$("#submit-contact")
+				.after('<i class="icon-spin4 animate-spin loader"></i>')
+				.attr("disabled", "disabled");
 
-		$("#message-contact").slideUp(750,function() {
-		$('#message-contact').hide();
-
- 		$('#submit-contact')
-			.after('<i class="icon-spin4 animate-spin loader"></i>')
-			.attr('disabled','disabled');
-			
-		$.post(action, {
-			name_contact: $('#name_contact').val(),
-			lastname_contact: $('#lastname_contact').val(),
-			email_contact: $('#email_contact').val(),
-			phone_contact: $('#phone_contact').val(),
-			message_contact: $('#message_contact').val(),
-			verify_contact: $('#verify_contact').val()
-		},
-			function(data){
-				document.getElementById('message-contact').innerHTML = data;
-				$('#message-contact').slideDown('slow');
-				$('#contactform .loader').fadeOut('slow',function(){$(this).remove()});
-				$('#submit-contact').removeAttr('disabled');
-				if(data.match('success') != null) $('#contactform').slideUp('slow');
-
-			}
-		);
-
+			$.post(
+				action,
+				{
+					name_contact: $("#name_contact").val(),
+					lastname_contact: $("#lastname_contact").val(),
+					email_contact: $("#email_contact").val(),
+					phone_contact: $("#phone_contact").val(),
+					message_contact: $("#message_contact").val(),
+					verify_contact: $("#verify_contact").val(),
+				},
+				function (data) {
+					document.getElementById("message-contact").innerHTML = data;
+					$("#message-contact").slideDown("slow");
+					$("#contactform .loader").fadeOut("slow", function () {
+						$(this).remove();
+					});
+					$("#submit-contact").removeAttr("disabled");
+					if (data.match("success") != null) $("#contactform").slideUp("slow");
+				}
+			);
 		});
 
 		return false;
-
 	});
-		});
-  /* ]]> */
+});
+/* ]]> */
